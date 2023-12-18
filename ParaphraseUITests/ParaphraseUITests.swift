93f4af4a73c856dd7e4e548f6e06dcf7775649cb
Juliano <alvarenga.vix@gmail.com>
2023-12-18 08:22:34 -0300

    Fastlane
    Fastlane - HWS Tutorial
    
    https://www.hackingwithswift.com/articles/98/how-to-streamline-your-development-with-fastlane

---
+++ ParaphraseUITests/ParaphraseUITests.swift
@@ -0,0 +1,45 @@
+//
+//  ParaphraseUITests.swift
+//  ParaphraseUITests
+//
+//  Created by Juliano Alvarenga on 16/12/23.
+//  Copyright © 2023 Hacking with Swift. All rights reserved.
+//
+
+import XCTest
+
+final class ParaphraseUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    @MainActor
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        snapshot("0Lauch")
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
